9457c3f3d73bae8514afc84d443dc4212ed78a08
EJJMC <44029567+EJJMC@users.noreply.github.com>
2019-12-14 16:54:58 +0000

    added music

---
+++ Game engines project/Assets/Audio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Audio : MonoBehaviour
+
+
+{
+    
+    AudioSource audiosource;
+   public static float[] samples = new float[512];
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audiosource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
