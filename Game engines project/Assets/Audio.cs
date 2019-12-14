d4130732ec57f9f31ee1006e8f531e234899a3f8
EJJMC <44029567+EJJMC@users.noreply.github.com>
2019-12-14 18:34:01 +0000

    ad

--- Game engines project/Assets/Audio.cs
+++ Game engines project/Assets/Audio.cs
@@ -10,6 +10,8 @@ public class Audio : MonoBehaviour
     
     AudioSource audiosource;
    public static float[] samples = new float[512];
+
+   public static float[] freqBand = new float[8];
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +22,34 @@ public class Audio : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource();
+        MakeFrequencyBands();
     }
 
     void GetSpectrumAudioSource()
     {
         audiosource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            float avarage = 0;
+            int SampleCount = (int) Mathf.Pow(2, i) * 2; // if its 0 , 2 will go to the power of 0
+        
+
+        for(int j =0;  j< SampleCount; j++)
+        {
+            avarage += samples[count] * (count + 1);
+            count++;
+        }
+
+        avarage /= count;
+
+        freqBand[i] = avarage * 10;
+
+        }
     }
 }
