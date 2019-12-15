e6847d9214d82b0b8e80638181504cd98f337327
EJJMC <44029567+EJJMC@users.noreply.github.com>
2019-12-15 22:01:07 +0000

    added non broken version of visuliser

---
+++ New Unity Project/Assets/Scripts/Audio.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class Audio : MonoBehaviour
+{
+    
+    
+        AudioSource audiosource;
+    public static float[] samples = new float[512];
+
+    public static float[] freqBand = new float[8];
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audiosource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            float avarage = 0;
+            int SampleCount = (int) Mathf.Pow(2, i) * 2; // if its 0 , 2 will go to the power of 0
+        
+
+            for(int j =0;  j< SampleCount; j++)
+            {
+                avarage += samples[count] * (count + 1);
+                count++;
+            }
+
+            avarage /= count;
+
+            freqBand[i] = avarage * 10;
+
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float force = 200;
+    [SerializeField] private Transform Ez;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+       // rb.AddForce(transform.forward*force*Time.deltaTime);
+        rb.velocity = transform.forward * force;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="EndZone")
+        {
+            this.gameObject.transform.position = Ez.transform.position;
+        }
+    }
+}
